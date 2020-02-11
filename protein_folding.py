47c6c341a21241ef3c0ac96699227725819c2b3a
szymslo <szymslo96@gmail.com>
2020-02-11 20:29:41 +0100

    v0.9 wykres, bez zliczania punktow

--- protein_folding.py
+++ protein_folding.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+biale_wykresX = []
+biale_wykresY = []
+czarne_wykresX = []
+czarne_wykresY = []
+wykresX = []
+wykresY = []
+
+
 def wyznaczkierunek(liczba):
     if liczba == 1:
         return "prawo"
@@ -15,6 +23,13 @@ def wyznaczkierunek(liczba):
         return "dol"
 
 
+'''
+def liczpunkty(tab):
+    for i in range(len(tab)):
+        for j in range(len(tab)):
+'''
+
+
 def zastaplitere(znak):
     if znak == 'p':  # biale
         return 1
@@ -22,92 +37,178 @@ def zastaplitere(znak):
         return 9
 
 
-def action(tablica, startpoint, rozmiar):
+def sprawdzlitere(znak):
+    if znak == 'p':  # biale
+        return True
+    elif znak == 'h':  # czarne
+        return False
+
+
+def action(rozmiar):
+    tablica = np.zeros((rozmiar, rozmiar), dtype=int)
+    startpoint = int(rozmiar / 2)
+
+    licznik_dol = 0
+    licznik_gora = 0
+    licznik_prawo = 0
+    licznik_lewo = 0
+
+    ostatnie_kierunki = ['brak', 'brak']
+
     # ZACZYNAMY OD ŚRODKA
     pozycjax = startpoint
     pozycjay = startpoint
     tablica[pozycjax][pozycjay] = zastaplitere(bialko[0])
 
+    wykresX.append(pozycjax)
+    wykresY.append(pozycjay)
+
+    if sprawdzlitere(bialko[0]):  # biale
+        biale_wykresX.append(pozycjax)
+        biale_wykresY.append(pozycjay)
+    else:  # czarne
+        czarne_wykresX.append(pozycjax)
+        czarne_wykresY.append(pozycjay)
+
     k = 1
     while k < rozmiar:
         kierunek = wyznaczkierunek(ra.randint(1, 4))
         if kierunek == 'dol':
-            if rozmiar - 1 > pozycjax + 1 > 0 and rozmiar - 1 > pozycjay > 0:
-                if tablica[pozycjax + 1][pozycjay] == 0:
-                    print(f'{k}->w dol')
-                    pozycjax = pozycjax + 1
-                    tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
-                    k += 1
-                    if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax - 1][pozycjay] == 9:
-                        print('łączenie...')
-                        # 8 to dwie czarne połączone
-                        tablica[pozycjax][pozycjay] = 8
-                        tablica[pozycjax - 1][pozycjay] = 8
-                    print(tablica)
-                else:
-                    print(f'{k}->kierunek {kierunek} zablokowany')
-                    continue
-            else:
-                print(f'{k}-> {kierunek} wyszło poza zakres')
+            if ostatnie_kierunki[-1] == 'dol' and ostatnie_kierunki[-2] == 'dol':
+                print("wymuszenie zmiany kierunku")
                 continue
+            else:
+                if rozmiar - 1 > pozycjax + 1 > 0 and rozmiar - 1 > pozycjay > 0:
+                    if tablica[pozycjax + 1][pozycjay] == 0:
+                        ostatnie_kierunki.append(kierunek)
+                        print(f'{k}->w dol')
+                        pozycjax = pozycjax + 1
+                        tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
+                        k += 1
+                        if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax - 1][pozycjay] == 9:
+                            print('łączenie...')
+                            # 8 to dwie czarne połączone
+                            tablica[pozycjax][pozycjay] = 8
+                            tablica[pozycjax - 1][pozycjay] = 8
+
+                        wykresX.append(pozycjax)
+                        wykresY.append(pozycjay)
+
+                        if tablica[pozycjax][pozycjay] == 1:
+                            biale_wykresX.append(pozycjax)
+                            biale_wykresY.append(pozycjay)
+                        elif tablica[pozycjax][pozycjay] == 9 or tablica[pozycjax][pozycjay] == 8:
+                            czarne_wykresX.append(pozycjax)
+                            czarne_wykresY.append(pozycjay)
+                    else:
+                        print(f'{k}->kierunek {kierunek} zablokowany')
+                        continue
+                else:
+                    print(f'{k}-> {kierunek} wyszło poza zakres')
+                    continue
         elif kierunek == 'gora':
-            if rozmiar - 1 > pozycjax - 1 > 0 and rozmiar - 1 > pozycjay > 0:
-                if tablica[pozycjax - 1][pozycjay] == 0:
-                    print(f'{k}->do gory')
-                    pozycjax = pozycjax - 1
-                    tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
-                    k += 1
-                    if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax + 1][pozycjay] == 9:
-                        print('łączenie...')
-                        # 8 to dwie czarne połączone
-                        tablica[pozycjax][pozycjay] = 8
-                        tablica[pozycjax + 1][pozycjay] = 8
-                    print(tablica)
-                else:
-                    print(f'{k}->kierunek {kierunek} zablokowany')
-                    continue
-            else:
-                print(f'{k}-> {kierunek} wyszło poza zakres')
+            if ostatnie_kierunki[-1] == 'gora' and ostatnie_kierunki[-2] == 'gora':
+                print("wymuszenie zmiany kierunku")
                 continue
+            else:
+                if rozmiar - 1 > pozycjax - 1 > 0 and rozmiar - 1 > pozycjay > 0:
+                    if tablica[pozycjax - 1][pozycjay] == 0:
+                        print(f'{k}->do gory')
+                        ostatnie_kierunki.append(kierunek)
+                        pozycjax = pozycjax - 1
+                        tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
+                        k += 1
+                        if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax + 1][pozycjay] == 9:
+                            print('łączenie...')
+                            # 8 to dwie czarne połączone
+                            tablica[pozycjax][pozycjay] = 8
+                            tablica[pozycjax + 1][pozycjay] = 8
+
+                        wykresX.append(pozycjax)
+                        wykresY.append(pozycjay)
+
+                        if tablica[pozycjax][pozycjay] == 1:
+                            biale_wykresX.append(pozycjax)
+                            biale_wykresY.append(pozycjay)
+                        elif tablica[pozycjax][pozycjay] == 9 or tablica[pozycjax][pozycjay] == 8:
+                            czarne_wykresX.append(pozycjax)
+                            czarne_wykresY.append(pozycjay)
+                    else:
+                        print(f'{k}->kierunek {kierunek} zablokowany')
+                        continue
+                else:
+                    print(f'{k}-> {kierunek} wyszło poza zakres')
+                    continue
         elif kierunek == 'prawo':
-            if rozmiar - 1 > pozycjax > 0 and rozmiar - 1 > pozycjay + 1 > 0:
-                if tablica[pozycjax][pozycjay + 1] == 0:
-                    print(f'{k}->w prawo')
-                    pozycjay = pozycjay + 1
-                    tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
-                    k += 1
-                    if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax][pozycjay - 1] == 9:
-                        print('łączenie...')
-                        # 8 to dwie czarne połączone
-                        tablica[pozycjax][pozycjay] = 8
-                        tablica[pozycjax][pozycjay - 1] = 8
-                    print(tablica)
-                else:
-                    print(f'{k}->kierunek {kierunek} zablokowany')
-                    continue
-            else:
-                print(f'{k}->{kierunek} wyszło poza zakres')
+            if ostatnie_kierunki[-1] == 'prawo' and ostatnie_kierunki[-2] == 'prawo':
+                print("wymuszenie zmiany kierunku")
                 continue
+            else:
+                if rozmiar - 1 > pozycjax > 0 and rozmiar - 1 > pozycjay + 1 > 0:
+                    if tablica[pozycjax][pozycjay + 1] == 0:
+                        print(f'{k}->w prawo')
+                        ostatnie_kierunki.append(kierunek)
+                        pozycjay = pozycjay + 1
+                        tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
+                        k += 1
+                        if tablica[pozycjax][pozycjay] == 9 and tablica[pozycjax][pozycjay - 1] == 9:
+                            print('łączenie...')
+                            # 8 to dwie czarne połączone
+                            tablica[pozycjax][pozycjay] = 8
+                            tablica[pozycjax][pozycjay - 1] = 8
+
+                        wykresX.append(pozycjax)
+                        wykresY.append(pozycjay)
+
+                        if tablica[pozycjax][pozycjay] == 1:
+                            biale_wykresX.append(pozycjax)
+                            biale_wykresY.append(pozycjay)
+                        elif tablica[pozycjax][pozycjay] == 9 or tablica[pozycjax][pozycjay] == 8:
+                            czarne_wykresX.append(pozycjax)
+                            czarne_wykresY.append(pozycjay)
+                    else:
+                        print(f'{k}->kierunek {kierunek} zablokowany')
+                        continue
+                else:
+                    print(f'{k}->{kierunek} wyszło poza zakres')
+                    continue
         elif kierunek == 'lewo':
-            if rozmiar - 1 > pozycjax > 0 and rozmiar - 1 > pozycjay - 1 > 0:
-                if tablica[pozycjax][pozycjay - 1] == 0:
-                    print('łączenie...')
-                    print(f'{k}->w lewo')
-                    pozycjay = pozycjay - 1
-                    tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
-                    k += 1
-                    if tablica[pozycjax][pozycjay] == 9 or 8 and tablica[pozycjax][pozycjay + 1] == 9:
-                        # 8 to dwie czarne połączone
-                        tablica[pozycjax][pozycjay] = 8
-                        tablica[pozycjax][pozycjay + 1] = 8
-                    print(tablica)
-                else:
-                    print(f'{k}->kierunek {kierunek} zablokowany')
-                    continue
-            else:
-                print(f'{k}->{kierunek} wyszło poza zakres')
+            if ostatnie_kierunki[-1] == 'lewo' and ostatnie_kierunki[-2] == 'lewo':
+                print("wymuszenie zmiany kierunku")
                 continue
+            else:
+                if rozmiar - 1 > pozycjax > 0 and rozmiar - 1 > pozycjay - 1 > 0:
+                    if tablica[pozycjax][pozycjay - 1] == 0:
+                        print('łączenie...')
+                        print(f'{k}->w lewo')
+                        ostatnie_kierunki.append(kierunek)
+                        pozycjay = pozycjay - 1
+                        tablica[pozycjax][pozycjay] = zastaplitere(bialko[k])
+                        k += 1
+                        if tablica[pozycjax][pozycjay] == 9 or 8 and tablica[pozycjax][pozycjay + 1] == 9:
+                            # 8 to dwie czarne połączone
+                            tablica[pozycjax][pozycjay] = 8
+                            tablica[pozycjax][pozycjay + 1] = 8
+
+                        wykresX.append(pozycjax)
+                        wykresY.append(pozycjay)
+
+                        if tablica[pozycjax][pozycjay] == 1:
+                            biale_wykresX.append(pozycjax)
+                            biale_wykresY.append(pozycjay)
+                        elif tablica[pozycjax][pozycjay] == 9 or tablica[pozycjax][pozycjay] == 8:
+                            czarne_wykresX.append(pozycjax)
+                            czarne_wykresY.append(pozycjay)
+                    else:
+                        print(f'{k}->kierunek {kierunek} zablokowany')
+                        continue
+                else:
+                    print(f'{k}->{kierunek} wyszło poza zakres')
+                    continue
     print(tablica)
+    print('------------------------------------------')
+    print('kierunki')
+    print(ostatnie_kierunki)
 
 
 regex = re.compile("^[ph]+$")
@@ -120,7 +221,22 @@ while not regex.match(bialko):
     bialko = input()
 
 dlugosc = len(bialko)
-temp = np.zeros((dlugosc, dlugosc), dtype=int)
-srodek = int(dlugosc / 2)
 
-action(temp, srodek, dlugosc)
+action(dlugosc)
+
+print('------------------------------------------')
+print('białe')
+print(biale_wykresX)
+print(biale_wykresY)
+print('------------------------------------------')
+print('czarne')
+print(czarne_wykresX)
+print(czarne_wykresY)
+print('------------------------------------------')
+
+plt.plot(wykresY, wykresX, 'k')
+plt.plot(biale_wykresY, biale_wykresX, 'co', czarne_wykresY, czarne_wykresX, 'ko', markersize=12)
+plt.grid(True)
+plt.axis([0, dlugosc, 0, dlugosc])
+plt.show()
+
